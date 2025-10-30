7be54797b32a5e0729a70ab9282dcd65090dc94f
Eduardo de Souza Cavalcanti Junior <eduardoscavalcantij@gmail.com>
2025-10-30 15:31:54 -0300

    funcao relatorio_medico

--- modulos/relatorios.py
+++ modulos/relatorios.py
@@ -1,5 +1,22 @@
+import gerenciador_dados
+
+ARQUIVO_CONSULTAS = "consultas.json"
+ARQUIVO_MEDICOS = "medicos.json" 
+
 def relatorio_medicos():
-    ...
+    banco_consultas = gerenciador_dados.carregar_dados(ARQUIVO_CONSULTAS)
+    banco_medicos = gerenciador_dados.carregar_dados(ARQUIVO_MEDICOS)
+
+    for m in banco_medicos:
+        consultas = 0
+        for c in banco_consultas:
+            if m['id'] == c['medicoid']:
+                consultas += 1
+        print('-' * 6, "RELATÓRIO", '-' * 6)
+        print(f"id: {m['id']}")
+        print(f"Médico: {m['nome']}")
+        print(f"Consultas agendadas: {consultas}")
+        print('-' * 23, '\n')
 
 def relatorio_data():
     ...
