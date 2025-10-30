b9733f4f4857e8648dd1cddf2e35d333df61a7bf
Eduardo de Souza Cavalcanti Junior <eduardoscavalcantij@gmail.com>
2025-10-30 19:49:00 -0300

    funcao relatorio_data

--- modulos/relatorios.py
+++ modulos/relatorios.py
@@ -17,6 +17,25 @@ def relatorio_medicos():
         print(f"Médico: {m['nome']}")
         print(f"Consultas agendadas: {consultas}")
         print('-' * 23, '\n')
+        
 
 def relatorio_data():
-    ...
+    banco_consultas = gerenciador_dados.carregar_dados(ARQUIVO_CONSULTAS)
+
+    datas_consultas = set()
+
+    for c in banco_consultas:
+        datas_consultas.add(c['data'])
+
+    datas = list(datas_consultas)
+    datas.sort()
+
+    for d in datas:
+        consultas = 0
+        for c in banco_consultas:
+            if d == c['data']:
+                consultas += 1
+        print('-' * 6, "RELATÓRIO", '-' * 6)
+        print(f"Data: {d}")
+        print(f"Consultas agendadas: {consultas}")
+        print('-' * 23, '\n')
