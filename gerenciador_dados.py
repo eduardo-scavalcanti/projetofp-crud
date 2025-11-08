8ddd64ccbf87961b68a3a663c07c2a5add527135
Eduardo de Souza Cavalcanti Junior <eduardoscavalcantij@gmail.com>
2025-11-08 11:17:05 -0300

    ajustes gerenciador_dados

--- gerenciador_dados.py
+++ gerenciador_dados.py
@@ -1,24 +1,26 @@
 import os
 import json
 
-CAMINHOABSOLUTO_DADOS = os.path.join(
+CAMINHO_ABSOLUTO_DADOS = os.path.join(
     os.path.dirname(__file__),
     "dados")
 
+def carregar_dados(nome_arquivo):
+    if not os.path.exists(CAMINHO_ABSOLUTO_DADOS):
+        os.mkdir("dados")
 
-def carregar_dados(nomearquivo):
-    caminho_completo = os.path.join(CAMINHOABSOLUTO_DADOS, nomearquivo)
+    caminho_completo = os.path.join(CAMINHO_ABSOLUTO_DADOS, nome_arquivo)
     
-    with open(caminho_completo, 'r', encoding='utf-8') as arquivo:
-        dados = json.load(arquivo)
-        return dados
+    if os.path.exists(caminho_completo):
+        with open(caminho_completo, 'r', encoding='utf-8') as arquivo:
+            dados = json.load(arquivo)
+            return dados
+    else:
+        return []
+    
 
+def salvar_dados(nome_arquivo, tabela):
+    caminho_completo = os.path.join(CAMINHO_ABSOLUTO_DADOS, nome_arquivo)
 
-def salvar_dados(nomearquivo, dicionario):
-    caminho_completo = os.path.join(CAMINHOABSOLUTO_DADOS, nomearquivo)
-    tabela = carregar_dados(nomearquivo)
-    
-    tabela.append(dicionario)
-    
     with open(caminho_completo, 'w', encoding='utf-8') as arquivo:
         json.dump(tabela, arquivo, ensure_ascii=False, indent=4)
