386177b957035eec6fb681d9697e0b70fb807e22
Eduardo de Souza Cavalcanti Junior <eduardoscavalcantij@gmail.com>
2025-11-13 16:56:33 -0300

    ajustes modulo relatorios

--- modulos/relatorios.py
+++ modulos/relatorios.py
@@ -1,52 +1,74 @@
 import gerenciador_dados
 
+ARQUIVO_MEDICOS = "medicos.json"
+ARQUIVO_PACIENTES = "pacientes.json"
 ARQUIVO_CONSULTAS = "consultas.json"
-ARQUIVO_MEDICOS = "medicos.json" 
 
 def relatorio_medicos():
     banco_medicos = gerenciador_dados.carregar_dados(ARQUIVO_MEDICOS)
-    banco_consultas = gerenciador_dados.carregar_dados(ARQUIVO_CONSULTAS)
 
     if len(banco_medicos) == 0:
-        print("Não há médicos cadastrados.")
+        print("ERRO! Não há médicos cadastrados.")
         return
-    elif len(banco_consultas) == 0:
-        print("Não há consultas agendadas.")
-        return
-    else:
-        for m in banco_medicos:
-            consultas = 0
-            for c in banco_consultas:
-                if m['id'] == c['id']:
-                    consultas += 1
-            print('-' * 6, "RELATÓRIO", '-' * 6)
-            print(f"id: {m['id']}")
-            print(f"Médico: {m['nome']}")
-            print(f"Consultas agendadas: {consultas}")
-            print('-' * 23, '\n')
-        
+    
+    banco_pacientes = gerenciador_dados.carregar_dados(ARQUIVO_PACIENTES)
 
-def relatorio_data():
+    if len(banco_pacientes) == 0:
+        print("ERRO! Não há médicos cadastrados.")
+        return
+    
     banco_consultas = gerenciador_dados.carregar_dados(ARQUIVO_CONSULTAS)
 
     if len(banco_consultas) == 0:
-        print("Não há consultas agendadas.")
+        print("ERRO! Não há médicos cadastrados.")
         return
-    else:
-        datas_consultas = set()
-
+    
+    for m in banco_medicos:
+        consultas = 0
         for c in banco_consultas:
-            datas_consultas.add(c['data'])
+            if m['crm'] == c['crm']:
+                consultas += 1
+        print('-' * 6, "RELATÓRIO", '-' * 6)
+        print(f"ID: {m['id']}")
+        print(f"Médico: {m['nome']}")
+        print(f"CRM: {m['crm']}")
+        print(f"Consultas agendadas: {consultas}")
+        print('-' * 23, '\n')
+        
 
-        datas = list(datas_consultas)
-        datas.sort()
+def relatorio_data():
+    banco_medicos = gerenciador_dados.carregar_dados(ARQUIVO_MEDICOS)
 
-        for d in datas:
-            consultas = 0
-            for c in banco_consultas:
-                if d == c['data']:
-                    consultas += 1
-            print('-' * 6, "RELATÓRIO", '-' * 6)
-            print(f"Data: {d}")
-            print(f"Consultas agendadas: {consultas}")
-            print('-' * 23, '\n')
+    if len(banco_medicos) == 0:
+        print("ERRO! Não há médicos cadastrados.")
+        return
+    
+    banco_pacientes = gerenciador_dados.carregar_dados(ARQUIVO_PACIENTES)
+
+    if len(banco_pacientes) == 0:
+        print("ERRO! Não há médicos cadastrados.")
+        return
+    
+    banco_consultas = gerenciador_dados.carregar_dados(ARQUIVO_CONSULTAS)
+
+    if len(banco_consultas) == 0:
+        print("ERRO! Não há médicos cadastrados.")
+        return
+
+    datas_consultas = set()
+
+    for c in banco_consultas:
+        datas_consultas.add(c['data'])
+
+    datas = list(datas_consultas)
+    datas.sort()
+
+    for d in datas:
+        consultas = 0
+        for c in banco_consultas:
+            if d == c['data']:
+                consultas += 1
+        print('-' * 6, "RELATÓRIO", '-' * 6)
+        print(f"Data: {d}")
+        print(f"Consultas agendadas: {consultas}")
+        print('-' * 23, '\n')
